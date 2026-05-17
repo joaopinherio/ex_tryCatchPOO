b3f4717e6315da8a5e6f0199af548e83c8b7c04d
joaopinheiro <joaopdepaula@gmail.com>
2026-05-17 17:11:21 -0300

    catalog class Agenda

---
+++ exTryCatch/src/dados/Agenda.java
@@ -0,0 +1,32 @@
+package dados;
+import java.util.ArrayList;
+
+public class Agenda implements Iterador{
+    private int posicaoAtual;
+    private ArrayList<Pessoa> colecao;
+
+    Agenda(){
+        colecao = new ArrayList<>();
+    }
+
+    @Override
+    public void reset(){
+        this.posicaoAtual = -1;
+    }
+
+    @Override
+    public boolean hasNext(){
+        if(colecao.get(posicaoAtual) != null){
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public Object next(){
+        return colecao.get(posicaoAtual+1);
+    }
+
+
+
+}
